36f9a13837248382dfe69f1027b2afad42181509
VuTrungHau <vuhau1312@gmail.com>
2022-05-25 17:12:13 +0700

    Hiện thực chức năng nhãn

---
+++ app/src/main/java/com/example/finalnoteapp/adapter/TagAdapter.java
@@ -0,0 +1,61 @@
+package com.example.finalnoteapp.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.finalnoteapp.R;
+import com.example.finalnoteapp.data.Note;
+import com.example.finalnoteapp.data.Tag;
+import com.example.finalnoteapp.fragment.HomeFragment;
+import com.example.finalnoteapp.fragment.NewReminderFragment;
+
+import java.util.List;
+
+public class TagAdapter extends RecyclerView.Adapter<TagAdapter.TagHolder>{
+
+    private NewReminderFragment context;
+    private List<Tag> tags;
+
+    public TagAdapter(NewReminderFragment context, List<Tag> tags){
+        this.context = context;
+        this.tags = tags;
+    }
+
+    @NonNull
+    @Override
+    public TagHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TagHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.tag_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TagHolder holder, int position) {
+        Tag tag = tags.get(position);
+        if(tag == null){
+            return;
+        }
+        holder.tagName.setText(tag.getTagName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(tags != null){
+            return tags.size();
+        }
+        return 0;
+    }
+
+
+    public static class TagHolder extends RecyclerView.ViewHolder {
+        TextView tagName;
+        public TagHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tagName = itemView.findViewById(R.id.tag_name);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/finalnoteapp/data/Tag.java
@@ -0,0 +1,18 @@
+package com.example.finalnoteapp.data;
+
+public class Tag {
+
+    private String tagName;
+
+    public Tag(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+}
